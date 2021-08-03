694659c93b2537c8adac6ff27d7a33500e0ffc7f
Tiffceet <zkang.loo@gmail.com>
2021-08-04 02:20:55 +0800

    Update main.cpp

--- JaegersPrototype/main.cpp
+++ JaegersPrototype/main.cpp
@@ -5,13 +5,13 @@
 int main(int argc, char** argv) {
 	// There can be only 1 instance of Engine because of opengl
 	// This instance can be accessed using EngineInstance static variable by including Engine.h
-	Engine e("Window Title here", 800, 800, 60);
+	EngineInstance = new Engine("Window Title here", 800, 800, 60);
 
-	e.registerKeyboardListener(KeyPressListener::listen);
+	EngineInstance->registerKeyboardListener(KeyPressListener::listen);
 
 	Rectangle r({ 0,0,0 }, { 60,{0,0,1} }, { 1,0,0 }, 1, 1);
-	e.Add(r);
+	EngineInstance->Add(r);
 
-	e.Start(argc, argv);
+	EngineInstance->Start(argc, argv);
 	return 0;
 }
