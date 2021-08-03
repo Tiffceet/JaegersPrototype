c9746f1f4f121ac9ce51ca1699df18cc92dc068e
Tiffceet <zkang.loo@gmail.com>
2021-08-04 01:34:59 +0800

    Add: Comment explaining class instance

--- JaegersPrototype/KeyPressListener.cpp
+++ JaegersPrototype/KeyPressListener.cpp
@@ -5,5 +5,5 @@
 void KeyPressListener::listen(unsigned char key, int x, int y) {
 	std::cout << "I heard something";
 	Object3D* o = EngineInstance->GetObject();
-	o->setPosition({0.2, 0,0});
+	o->setPosition({ o->getPosition().x + 0.1f, 0,0 });
 }
--- JaegersPrototype/main.cpp
+++ JaegersPrototype/main.cpp
@@ -3,14 +3,15 @@
 #include "Rectangle.h"
 #include "KeyPressListener.h"
 int main(int argc, char** argv) {
+	// There can be only 1 instance of Engine because of opengl
+	// This instance can be accessed using EngineInstance static variable by including Engine.h
 	Engine e("Window Title here", 800, 800, 60);
 
-	// KeyPressListener::Init(&e);
 	e.registerKeyboardListener(KeyPressListener::listen);
 
 	Rectangle r({ 0,0,0 }, { 60,{0,0,1} }, { 1,0,0 }, 1, 1);
-	// Rectangle r;
 	e.Add(r);
+
 	e.Start(argc, argv);
 	return 0;
 }
