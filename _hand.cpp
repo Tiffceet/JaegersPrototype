b98c2b29f5fad555057214f1579c636c75f8a5dd
Tiffceet <zkang.loo@gmail.com>
2021-09-08 17:49:24 +0800

    Update _hand.cpp

--- _hand.cpp
+++ _hand.cpp
@@ -3,11 +3,27 @@
 #include "typedefs.h"
 #include "primitives.h"
 #include "texture.h"
-
+int hand_frame = 0;
 void drawRobotHand(Prop3D props) {
     glPushMatrix();
     useTexture(-1);
-    // applyProps(props);
-    drawCube(props, {1,1,1});
+    glColor3f(1,1,1);
+    Prop3D upper_joint;
+    upper_joint.origin.y = -2.5;
+    upper_joint.rot.z = 30;
+    drawCube(upper_joint, {2,5,2});
+
+    Prop3D upper_arm; 
+    upper_arm.pos = {2.5, -4, 0};
+    upper_arm.rot.x = 90;
+    drawCylinder(upper_arm, 1, 2, 4, 30, 30);
+
+    Prop3D lower_arm;
+    lower_arm.pos = {2.5, -8, 0};
+    lower_arm.rot.x = 90;
+    drawCylinder(lower_arm, 2, 1, 4, 30, 30);
+
+    glColor3f(1,0,0);
+
     glPopMatrix();
 }
