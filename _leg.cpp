c7042d94319cb44b54eb3148943b39685811f99a
Tiffceet <zkang.loo@gmail.com>
2021-09-16 22:53:37 +0800

    Add: Tex coords for leg badge

--- _leg.cpp
+++ _leg.cpp
@@ -226,17 +226,26 @@ void drawOctoid(Prop3D props, float thickness)
     Vec3f b_8 = {1.1 * cos(315 * M_PI / 180.0), 1.1 * sin(315 * M_PI / 180.0), -thickness / 2};
 
     useDefaultTexture();
+    useTexture("box");
     glColor3f(1, 1, 1);
     glBegin(GL_POLYGON);
     Vec3f norm = calculate_normal(f_1, f_2, f_3);
     glNormal3f(norm.x, norm.y, norm.z);
+    glTexCoord2f(f_1.x, f_1.y);
     glVertex3f(f_1.x, f_1.y, f_1.z);
+    glTexCoord2f(f_2.x, f_2.y);
     glVertex3f(f_2.x, f_2.y, f_2.z);
+    glTexCoord2f(f_3.x, f_3.y);
     glVertex3f(f_3.x, f_3.y, f_3.z);
+    glTexCoord2f(f_4.x, f_4.y);
     glVertex3f(f_4.x, f_4.y, f_4.z);
+    glTexCoord2f(f_5.x, f_5.y);
     glVertex3f(f_5.x, f_5.y, f_5.z);
+    glTexCoord2f(f_6.x, f_6.y);
     glVertex3f(f_6.x, f_6.y, f_6.z);
+    glTexCoord2f(f_7.x, f_7.y);
     glVertex3f(f_7.x, f_7.y, f_7.z);
+    glTexCoord2f(f_8.x, f_8.y);
     glVertex3f(f_8.x, f_8.y, f_8.z);
     glEnd();
 
@@ -244,13 +253,21 @@ void drawOctoid(Prop3D props, float thickness)
     norm = calculate_normal(b_1, b_2, b_3);
     norm = -norm;
     glNormal3f(norm.x, norm.y, norm.z);
+    glTexCoord2f(f_1.x, f_1.y);
     glVertex3f(b_1.x, b_1.y, b_1.z);
+    glTexCoord2f(f_2.x, f_2.y);
     glVertex3f(b_2.x, b_2.y, b_2.z);
+    glTexCoord2f(f_3.x, f_3.y);
     glVertex3f(b_3.x, b_3.y, b_3.z);
+    glTexCoord2f(f_4.x, f_4.y);
     glVertex3f(b_4.x, b_4.y, b_4.z);
+    glTexCoord2f(f_5.x, f_5.y);
     glVertex3f(b_5.x, b_5.y, b_5.z);
+    glTexCoord2f(f_6.x, f_6.y);
     glVertex3f(b_6.x, b_6.y, b_6.z);
+    glTexCoord2f(f_7.x, f_7.y);
     glVertex3f(b_7.x, b_7.y, b_7.z);
+    glTexCoord2f(f_8.x, f_8.y);
     glVertex3f(b_8.x, b_8.y, b_8.z);
     glEnd();
 
@@ -259,9 +276,13 @@ void drawOctoid(Prop3D props, float thickness)
     norm = calculate_normal(b_1, f_1, f_2);
     glNormal3f(norm.x, norm.y, norm.z);
     glNormal3f(-norm.x, -norm.y, -norm.z);
+    glTexCoord2f(0, 0);
     glVertex3f(b_1.x, b_1.y, b_1.z);
+    glTexCoord2f(0, 1);
     glVertex3f(f_1.x, f_1.y, f_1.z);
+    glTexCoord2f(1, 1);
     glVertex3f(f_2.x, f_2.y, f_2.z);
+    glTexCoord2f(1, 0);
     glVertex3f(b_2.x, b_2.y, b_2.z);
     glEnd();
 
@@ -269,9 +290,13 @@ void drawOctoid(Prop3D props, float thickness)
     norm = calculate_normal(b_2, f_2, f_3);
     glNormal3f(norm.x, norm.y, norm.z);
     glNormal3f(-norm.x, -norm.y, -norm.z);
+    glTexCoord2f(0, 0);
     glVertex3f(b_2.x, b_2.y, b_2.z);
+    glTexCoord2f(0, 1);
     glVertex3f(f_2.x, f_2.y, f_2.z);
+    glTexCoord2f(1, 1);
     glVertex3f(f_3.x, f_3.y, f_3.z);
+    glTexCoord2f(1, 0);
     glVertex3f(b_3.x, b_3.y, b_3.z);
     glEnd();
 
@@ -279,9 +304,13 @@ void drawOctoid(Prop3D props, float thickness)
     norm = calculate_normal(b_3, f_3, f_4);
     glNormal3f(norm.x, norm.y, norm.z);
     glNormal3f(-norm.x, -norm.y, -norm.z);
+    glTexCoord2f(0, 0);
     glVertex3f(b_3.x, b_3.y, b_3.z);
+    glTexCoord2f(0, 1);
     glVertex3f(f_3.x, f_3.y, f_3.z);
+    glTexCoord2f(1, 1);
     glVertex3f(f_4.x, f_4.y, f_4.z);
+    glTexCoord2f(1, 0);
     glVertex3f(b_4.x, b_4.y, b_4.z);
     glEnd();
 
@@ -289,9 +318,13 @@ void drawOctoid(Prop3D props, float thickness)
     norm = calculate_normal(b_4, f_4, f_5);
     glNormal3f(norm.x, norm.y, norm.z);
     glNormal3f(-norm.x, -norm.y, -norm.z);
+    glTexCoord2f(0, 0);
     glVertex3f(b_4.x, b_4.y, b_4.z);
+    glTexCoord2f(0, 1);
     glVertex3f(f_4.x, f_4.y, f_4.z);
+    glTexCoord2f(1, 1);
     glVertex3f(f_5.x, f_5.y, f_5.z);
+    glTexCoord2f(1, 0);
     glVertex3f(b_5.x, b_5.y, b_5.z);
     glEnd();
 
@@ -299,9 +332,13 @@ void drawOctoid(Prop3D props, float thickness)
     norm = calculate_normal(b_5, f_5, f_6);
     glNormal3f(norm.x, norm.y, norm.z);
     glNormal3f(-norm.x, -norm.y, -norm.z);
+    glTexCoord2f(0, 0);
     glVertex3f(b_5.x, b_5.y, b_5.z);
+    glTexCoord2f(0, 1);
     glVertex3f(f_5.x, f_5.y, f_5.z);
+    glTexCoord2f(1, 1);
     glVertex3f(f_6.x, f_6.y, f_6.z);
+    glTexCoord2f(1, 0);
     glVertex3f(b_6.x, b_6.y, b_6.z);
     glEnd();
 
@@ -309,9 +346,13 @@ void drawOctoid(Prop3D props, float thickness)
     norm = calculate_normal(b_6, f_6, f_7);
     glNormal3f(norm.x, norm.y, norm.z);
     glNormal3f(-norm.x, -norm.y, -norm.z);
+    glTexCoord2f(0, 0);
     glVertex3f(b_6.x, b_6.y, b_6.z);
+    glTexCoord2f(0, 1);
     glVertex3f(f_6.x, f_6.y, f_6.z);
+    glTexCoord2f(1, 1);
     glVertex3f(f_7.x, f_7.y, f_7.z);
+    glTexCoord2f(1, 0);
     glVertex3f(b_7.x, b_7.y, b_7.z);
     glEnd();
 
@@ -319,9 +360,13 @@ void drawOctoid(Prop3D props, float thickness)
     norm = calculate_normal(b_7, f_7, f_8);
     glNormal3f(norm.x, norm.y, norm.z);
     glNormal3f(-norm.x, -norm.y, -norm.z);
+    glTexCoord2f(0, 0);
     glVertex3f(b_7.x, b_7.y, b_7.z);
+    glTexCoord2f(0, 1);
     glVertex3f(f_7.x, f_7.y, f_7.z);
+    glTexCoord2f(1, 1);
     glVertex3f(f_8.x, f_8.y, f_8.z);
+    glTexCoord2f(1, 0);
     glVertex3f(b_8.x, b_8.y, b_8.z);
     glEnd();
 
@@ -329,9 +374,13 @@ void drawOctoid(Prop3D props, float thickness)
     norm = calculate_normal(b_8, f_8, b_1);
     glNormal3f(norm.x, norm.y, norm.z);
     glNormal3f(-norm.x, -norm.y, -norm.z);
+    glTexCoord2f(0, 0);
     glVertex3f(b_8.x, b_8.y, b_8.z);
+    glTexCoord2f(0, 1);
     glVertex3f(f_8.x, f_8.y, f_8.z);
+    glTexCoord2f(1, 1);
     glVertex3f(f_1.x, f_1.y, f_1.z);
+    glTexCoord2f(1, 0);
     glVertex3f(b_1.x, b_1.y, b_1.z);
     glEnd();
 
